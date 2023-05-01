bdb8bd64ecca882c3e3918fe142f7626593392e2
Konstantin Motuskov <motuskov@gmail.com>
2023-05-01 12:14:45 -0300

    Implement menu usage example

--- test_tree_menu/test_tree_menu/urls.py
+++ test_tree_menu/test_tree_menu/urls.py
@@ -15,7 +15,65 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html')),
+    path('pets/<url>', TemplateView.as_view(template_name='index.html')),
+    path('pets/dogs/<url>', TemplateView.as_view(template_name='index.html')),
+    path('pets/cats/<url>', TemplateView.as_view(template_name='index.html')),
+    path(
+        'pets/dogs/shepherd/<url>',
+        TemplateView.as_view(template_name='index.html')
+    ),
+    path(
+        'cities/russia',
+        TemplateView.as_view(template_name='index.html'),
+        name='russia'
+    ),
+    path(
+        'cities/russia/moscow',
+        TemplateView.as_view(template_name='index.html'),
+        name='moscow'
+    ),
+    path(
+        'cities/russia/murmansk',
+        TemplateView.as_view(template_name='index.html'),
+        name='murmansk'
+    ),
+    path(
+        'cities/russia/yaroslavl',
+        TemplateView.as_view(template_name='index.html'),
+        name='yaroslavl'
+    ),
+    path(
+        'cities/australia',
+        TemplateView.as_view(template_name='index.html'),
+        name='australia'
+    ),
+    path(
+        'cities/australia/perth',
+        TemplateView.as_view(template_name='index.html'),
+        name='perth'
+    ),
+    path(
+        'cities/australia/melbourne',
+        TemplateView.as_view(template_name='index.html'),
+        name='melbourne'
+    ),
+    path(
+        'cities/australia/sydney',
+        TemplateView.as_view(template_name='index.html'),
+        name='sydney'
+    ),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
